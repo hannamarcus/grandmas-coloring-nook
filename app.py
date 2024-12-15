7213e76c919e59a51d440ee469106ace87205a53
hannamarcus <hannamarcus1@gmail.com>
2024-12-15 12:55:06 -0800

    added port

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def index():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
